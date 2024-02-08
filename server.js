16db657d4539a8d0f38ca15740f677e76b44e972
yoav1870 <yoav18700@gmail.com>
2024-02-08 13:00:27 +0200

    with logger

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const logger = require("morgan");
+const fs = require("fs");
+const path = require("path");
 const { planRouter } = require("./router/plan.router");
 const { NotFoundUrlError } = require("./errors/errors");
+const AllLogs = fs.createWriteStream(path.join(__dirname, "logs.log"), {
+  flags: "a",
+});
 const app = express();
 const port = process.env.PORT || 3000;
 module.exports = app;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+  logger("common", {
+    stream: AllLogs,
+  })
+);
 app.use("/api/plans", planRouter);
 app.use((req, res, next) => {
   next(new NotFoundUrlError());
