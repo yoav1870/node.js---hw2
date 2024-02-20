5b2cb692236f1c13ab62a997abfb13919b1e6dcf
yoav1870 <yoav18700@gmail.com>
2024-02-20 20:00:21 +0200

    Removing options from  CORS option

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specified methods
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specified methods
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
